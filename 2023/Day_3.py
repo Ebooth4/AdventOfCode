b4f000aea24ccf0b2dad2bb958686a9e5a50edf1
Ebooth4 <ebootheepic24@gmail.com>
2023-12-06 18:39:04 -0600

    set up day 3 file

---
+++ 2023/Day_3.py
@@ -0,0 +1,13 @@
+from aocd import get_data, submit
+
+data = get_data(day=3, year=2023)# .splitlines()
+# print(data)
+def Part1():
+    return 0
+
+def Part2():
+    return 0
+
+# print(f"Part 1: {Part1()}, Part 2: {Part2()}")
+# submit(Part1(), day=1, year=2023)
+# submit(Part2(), day=1, year=2023)
