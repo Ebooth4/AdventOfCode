250feba7b40f37dd2b16ae5f24f4f4eac0871522
Evan Graham <douglas.e.graham@dynetics.com>
2024-12-02 11:48:40 -0600

    day 1 and 2 done

---
+++ 2024/Day_1.py
@@ -0,0 +1,29 @@
+from aocd import get_data, submit
+
+session_id = ""
+
+data = get_data(session=session_id, day=1, year=2024).splitlines()
+left = []
+right = []
+for line in data:
+    left.append(int(line.split()[0]))
+    right.append(int(line.split()[1]))
+
+def Part1():
+    total_difference = 0
+    left.sort()
+    right.sort()
+    for i in range(len(left)):
+        total_difference += abs(left[i] - right[i])
+    return total_difference
+
+def Part2():
+    similarity_score = 0
+    for i in range(len(left)):
+        occurances = right.count(left[i])
+        similarity_score += (left[i] * occurances)
+    return similarity_score
+
+print(f"Part 1: {Part1()}, Part 2: {Part2()}")
+# submit(Part1(), session=session_id, day=1, year=2024)
+submit(Part2(), session=session_id, day=1, year=2023)
---
+++ 2024/Day_2.py
@@ -0,0 +1,97 @@
+from aocd import get_data, submit
+
+session_id = ""
+
+data = get_data(session=session_id, day=2, year=2024).splitlines()
+# file = open("data.txt")
+# data= file.read().splitlines()
+def Part1():
+    # numbers = []
+    
+    # result = 0
+    # for line in data:
+    #     safe = True
+    #     numbers = list(map(int, line.split()))
+        
+    #     # print(numbers)
+    #     if numbers[0] < numbers[1]:
+    #         # Increasing
+    #         for i in range(len(numbers)):
+    #             if i != len(numbers) - 1:
+    #                 if numbers[i] >= numbers[i+1]:
+    #                     # print("Direction Change!")
+    #                     safe = False
+    #                     break
+                    
+    #                 if abs(numbers[i+1] - numbers[i]) > 3:
+    #                     # print("Big Step!")
+    #                     safe = False
+    #                     break
+                
+    #     else:
+    #         # Decreasing
+    #         for i in range(len(numbers)):
+    #             if i != len(numbers) - 1:
+    #                 if numbers[i] <= numbers[i+1]:
+    #                     # print("Direction Change!")
+    #                     safe = False
+    #                     break
+                    
+    #                 if abs(numbers[i+1] - numbers[i]) > 3:
+    #                     # print("Big Step!")
+    #                     safe = False
+    #                     break
+    #     # print("Safe? - " + str(safe))
+    #     if safe:
+    #         result += 1
+    # return result
+    return 0
+
+def test(numbers, remove):
+    del numbers[remove]
+    i = 0
+    safe = True
+    while i != len(numbers) - 1:
+        increasing = (numbers[0] <= numbers[1])
+        print(str(numbers[i]) + " : " + str(numbers[i+1]))
+        
+        if numbers[i] >= numbers[i+1] and increasing:
+            print("Direction Change!")
+            safe = False
+            break
+        elif numbers[i] <= numbers[i+1] and not increasing:
+            print("Direction Change!")
+            safe = False
+            break
+        
+        if abs(numbers[i+1] - numbers[i]) > 3:
+            print("Big Step!")
+            safe = False
+            break
+        i += 1
+    print(safe)
+    return safe
+
+def Part2():
+    numbers = []
+    
+    result = 0
+    for line in data:
+        dampened = 0
+        safe = False
+        numbers = list(map(int, line.split()))
+        i = 0
+        print(numbers)
+        for i in range(len(numbers)):
+            if test(numbers.copy(), i):
+                safe = True
+                break
+    
+        print("Safe? - " + str(safe))
+        if safe:
+            result += 1
+    return result
+
+print(f"Part 1: {Part1()}, Part 2: {Part2()}")
+# submit(Part1(), session=session_id, day=2, year=2024)
+# submit(Part2(), session=session_id, day=2, year=2024)
