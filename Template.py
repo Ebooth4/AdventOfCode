5c2d4bd71d861ef2023d574cd79c905df3e9c7b9
Evan Graham <douglas.e.graham@dynetics.com>
2024-12-03 10:56:18 -0600

    year fix

--- Template.py
+++ Template.py
@@ -5,7 +5,7 @@ from aocd import get_data, submit
 
 session_id = getSessionId()
 
-data = get_data(session=session_id, day=1, year=2023).splitlines()
+data = get_data(session=session_id, day=1, year=2024).splitlines()
 
 def Part1():
     return 0
@@ -14,5 +14,5 @@ def Part2():
     return 0
 
 print(f"Part 1: {Part1()}, Part 2: {Part2()}")
-# submit(Part1(), session=session_id, day=1, year=2023)
-# submit(Part2(), session=session_id, day=1, year=2023)
+# submit(Part1(), session=session_id, day=1, year=2024)
+# submit(Part2(), session=session_id, day=1, year=2024)
