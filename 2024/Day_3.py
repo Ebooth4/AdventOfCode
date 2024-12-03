1bf3e13638e8dec3c2ec105bc1ae8bbcbba63734
Evan Graham <douglas.e.graham@dynetics.com>
2024-12-03 10:21:10 -0600

    fixed day3

--- 2024/Day_3.py
+++ 2024/Day_3.py
@@ -4,13 +4,9 @@ import re
 session_id = ""
 
 data = get_data(session=session_id, day=3, year=2024)
-data = "do()" + str(data) + "don't()"
+data = "do()" + data + "don't()"
 
-testing_file = open("test.txt")
-test_data = testing_file.read()
-
-
-def Part1():
+def Part1(data = data):
     sum = 0
     valid_muls = re.findall(r"mul\(\d+,\d+\)", data)
     for mul in valid_muls:
@@ -20,18 +16,8 @@ def Part1():
     return sum
 
 def Part2():
-    # BROKEN, My Regex doesn't work for some reason
-    # Got the answer by using VSCodes regexto format .txt file and ran normal part1 stuff of formatted input data
-    sum = 0
-    # do_and_donts = re.findall(r"do\(\).*?don't\(\)", data.strip("\n"))
-    # print(len(do_and_donts))
-        
-    valid_muls = re.findall(r"mul\(\d+,\d+\)", "".join(test_data.strip("\n")))
-    for mul in valid_muls:
-        numbers = str(mul).split("(")[1].strip(")")
-        left, right = numbers.split(",")
-        sum += int(left) * int(right)
-    return sum
+    do_and_donts = re.findall(r"do\(\).*?don't\(\)", ("".join(data)).strip("\n"), flags=re.DOTALL)
+    return Part1("".join(do_and_donts))
 
 print(f"Part 1: {Part1()}, Part 2: {Part2()}")
 # submit(Part1(), session=session_id, day=3, year=2024)
